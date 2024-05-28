a38219da51a9f714442882929a21d0188371ee2f
oswaldo chilin <oswaldochilin49@gmail.com>
2024-05-28 11:14:16 -0600

    se hico el ejercicio 3 con los metodos para agregar u mostrar los libros

---
+++ src/ejercicio3/App.java
@@ -0,0 +1,15 @@
+package ejercicio3;
+
+public class App extends libro{
+    public static void main(String[] args) {
+
+        libro libro = new  libro();
+        libro libro2 = new libro(1, "titulo", "guitierrez", "armando");
+        libro libro3 = new libro(2, "titulo", "guitierrez", "armando");
+
+        libro.addBook(libro2);
+        libro.addBook(libro3);
+
+        libro.toListBook();
+    }
+}
---
+++ src/ejercicio3/libro.java
@@ -0,0 +1,73 @@
+package ejercicio3;
+
+import java.util.LinkedHashSet;
+
+/*DESARROLLAR UN PROGRAMA QUE PERMITA ALMACENAR MULRIPLES ELEMENTOS DE LA
+ CADENA DE LA CLASE LIBRO LA CLASE LIBRO VA A TENER LOS
+  SIGUIENTE (Id,Titulo,Edicion,Autor) EN UN LINKEDHASHSET*/
+
+
+public class libro {
+
+    private int id;
+    private String titulo;
+    private String edicion;
+    private String autor;
+
+    LinkedHashSet<libro> Libros = new LinkedHashSet<>();
+
+    public libro() {
+        super();
+    }
+
+    public libro(int id, String titulo, String edicion, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.edicion = edicion;
+        this.autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getEdicion() {
+        return edicion;
+    }
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void addBook(libro libro) {
+        
+        Libros.add(libro);
+
+        System.out.println("se agrego el libro");
+    }
+
+    
+    public void toListBook() {
+
+        for (libro libro : Libros) {
+            
+          System.out.println("id: " + libro.getId() + " Titulo: " + libro.getTitulo()
+          + " Edicion: " + libro.getEdicion() + " Autor: " + libro.getAutor());
+        }
+    }
+
+     
+}
