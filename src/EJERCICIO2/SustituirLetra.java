8ad9d89e13fe428c3b7c71b69de14589ca5d2e42
Nery David Mendez Mendez <nerydavidmendez@gmail.com>
2024-05-28 10:59:59 -0600

    se elimino la clase mayuscular para poner la funcion en la clase main de la carpeta EJERCICIO2 donde esta la funcion para cambiar la primera letra de minuscula a mayuscula

--- src/EJERCICIO2/SustituirLetra.java
+++ src/EJERCICIO2/SustituirLetra.java
@@ -3,5 +3,32 @@ package EJERCICIO2;
 public class SustituirLetra {
     public static void main(String[] args) {
         
+        String input = "me gusta la musica jazz";
+        
+        
+        String result = Mayuscula(input);
+        
+        System.out.println("Resultado: " + result);
     }
+
+    private static String Mayuscula(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+
+        boolean capitalizeNext = true;
+        for (char ch : str.toCharArray()) {
+            if (capitalizeNext && Character.isLetter(ch)) {
+                result.append(Character.toUpperCase(ch));
+                capitalizeNext = false;
+            } else {
+                result.append(ch);
+            }
+            
+            if (ch == ' ') {
+                capitalizeNext = true;
+            }
+        }
+        
+        return result.toString();
+    }
+    
 }
